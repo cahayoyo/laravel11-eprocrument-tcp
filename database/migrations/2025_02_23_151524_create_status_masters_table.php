607a27a300d8580d4a78b60f601d95224dd4bc3d
cahayoyo <cahayoyo@gmail.com>
2025-02-23 15:23:46 +0700

    add tender_categories, status_masters, material_categories migration

---
+++ database/migrations/2025_02_23_151524_create_status_masters_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('status_masters', function (Blueprint $table) {
+            $table->id();
+            $table->enum('type', ['tender', 'proposal', 'contract', 'payment']);
+            $table->string('name', 50);
+            $table->string('description')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('status_masters');
+    }
+};
