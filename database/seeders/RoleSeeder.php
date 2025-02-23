eb8194a9bbee50e8ed4beb3a1c554229fc25ac07
cahayoyo <cahayoyo@gmail.com>
2025-02-23 11:22:36 +0700

    add roleseeder

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,31 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $roles = [
+            [
+                'name' => 'Admin',
+                'description' => 'Super Admin with all permissions'
+            ],
+            [
+                'name' => 'Procurement Staff',
+                'description' => 'Staff for managing procurement process'
+            ],
+            [
+                'name' => 'Vendor',
+                'description' => 'External vendor/supplier'
+            ],
+            [
+                'name' => 'Project Manager',
+                'description' => 'Manager for handling projects'
+            ],
+            [
+                'name' => 'Finance',
+                'description' => 'Staff for managing financial aspects'
+            ]
+        ];
+
+        foreach ($roles as $role) {
+            Role::create($role);
+        }
     }
 }
